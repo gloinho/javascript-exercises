146b121fc4b361295d2428b1d1a75f4039cc4454
Anastacio <gl0inn@hotmail.com>
2022-06-15 14:29:52 -0300

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum=0
+    if(first <0 || second<0 || typeof(first)!='number' || typeof(second)!='number'){
+        return 'ERROR'
+    }
+    else if (second>first){
+        for (i=first; i <= second ; i++){
+            sum += i
+        };
+    }
+    else if (second<first){
+        for (i=second; i <= first ; i++){
+            sum +=i
+        };
+    };
+    return sum
+}
 
-};
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
