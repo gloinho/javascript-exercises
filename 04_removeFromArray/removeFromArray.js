300c707340c862d522b36539ad9df0a0cd9c9a1b
Anastacio <gl0inn@hotmail.com>
2022-06-15 13:58:25 -0300

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let array = args[0];
+    let values = args.splice(1);
 
-};
+    for (let value of values){
+        let index = array.findIndex(arg => arg === value)
+        if (index != -1){array.splice(index, 1)};
+        };
+    return array;
+    };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
