bd7f6673fce53470cc2872e85a179cae1fe07a1b
Anastacio <gl0inn@hotmail.com>
2022-06-15 14:49:55 -0300

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  return Math.round((temp-32)*(5/9)*10)/10
+//	[°C] = ([°F] − 32) × 5⁄9
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  return Math.round((temp*(9/5)+32)*10)/10
 
+//[°F] = [°C] × 9⁄5 + 32
 };
 
 // Do not edit below this line
