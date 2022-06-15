332481bac3531b25242fac6ac79437663b3f29cc
Anastacio <gl0inn@hotmail.com>
2022-06-15 11:22:40 -0300

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, times) {
+    if (times < 0){
+        return 'ERROR'
+    }
+    let repetition = phrase.repeat(times)
+    return repetition
 };
 
 // Do not edit below this line
