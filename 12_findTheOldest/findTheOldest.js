da5109f1db15f804846658d4f68342097e29386a
Anastacio <gl0inn@hotmail.com>
2022-06-23 11:47:46 -0300

    solved with filter and reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(group) {
+    let oldest = group.reduce((old, person) => {
+        if(!person.yearOfDeath){person.yearOfDeath = (new Date).getFullYear()};
+        let seeAge = person.yearOfDeath-person.yearOfBirth
+        if (old < seeAge) {old = seeAge};
+        return old
+    }, 0)
+    let whoIsOldest = group.filter(person => (person.yearOfDeath-person.yearOfBirth) === oldest)[0]
+    return whoIsOldest
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
