abe4c18415da05131ead46e595bb8712d2472631
Anastacio <gl0inn@hotmail.com>
2022-06-23 10:48:34 -0300

    solved with recursive function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let number = Number(input)
+    if (number<0){return "OOPS"}
+    else if (number===1){return 1}
+    else if (number===0){return 0}
+    else {return(fibonacci(number-1) + fibonacci(number-2))}
+    
 
 };
 
