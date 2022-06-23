e95785f67eaf960b8eee50105eeee7a286790cf7
Anastacio <gl0inn@hotmail.com>
2022-06-23 10:27:25 -0300

    solved with regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let onlyletters = word.replace(/[^\w]/gi, '').toLowerCase()
+    let reversed = onlyletters.split('').reverse().join('')
+    return onlyletters === reversed
 };
 
 // Do not edit below this line
